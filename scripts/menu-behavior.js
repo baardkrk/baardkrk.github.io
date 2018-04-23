0c74cca56d8213e111b7fab11dbc148b74fe8475
baardkrk <baard.krohg@gmail.com>
2018-04-23 13:28:19 +0900

    added old website

---
+++ scripts/menu-behavior.js
@@ -0,0 +1,8 @@
+function top_menu_collapse() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
