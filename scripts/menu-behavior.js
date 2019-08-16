6cfcce4ceb24094081b62346e932e637ad12a584
Bård-Kristian Krohg <baard.krohg@gmail.com>
2019-08-16 17:29:26 +0200

    made jekyll site

--- scripts/menu-behavior.js
+++
@@ -1,8 +0,0 @@
-function top_menu_collapse() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
